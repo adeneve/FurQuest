e4d8b713e4d820c897c3fc290f8548b6b7a38ee5
adeneve831 <adeneve83@gmail.com>
2020-09-25 21:06:55 -0700

    added sprite sheet

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 import DatabaseController from './database.js'
 import Controller from './controller.js'
 import Display from './display.js'
+import SpriteSheet from './SpriteSheet.js'
+import {loadImage} from './loaders.js'
 
 const dbc = new DatabaseController();
 var gameCanvas = document.getElementById("gameCanvas") 
-const display = new Display(gameCanvas, 1024, 800)
+const display = new Display(gameCanvas, 1024, 800) 
 const controller = new Controller(gameCanvas)
+console.log("hello?")
 display.loadScene()
 
+
+loadImage('../assets/tiles.png')
+.then(image => {
+    console.log('wut')
+    const sprites = new SpriteSheet(image);
+    sprites.define('ground', 0, 0);
+    sprites.define('sky', 3, 23);
+    sprites.draw('ground', gameCanvas.getContext('2d'), 16, 16)
+});
