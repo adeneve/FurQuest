3e7b3577e09549a005a909ed10e2d630b8c16d6d
adeneve831 <adeneve83@gmail.com>
2020-09-26 21:43:34 -0700

    working on animation

---
+++ js/Animator.js
@@ -0,0 +1,21 @@
+export default class Animator{
+
+    animating = false
+    lastUpdateTime = -1
+    
+    constructor(spriteSheet, animationSteps, speed){
+        this.spriteSheet = spriteSheet
+        this.currentStep = 0
+        this.animationSteps = animationSteps
+        this.speed = speed
+        this.start = -1
+        this.frameNames = spriteSheet.tiles.keys()
+        this.animating = false
+        this.lastUpdateTime = -1
+    }
+
+    getNextSprite(){
+        this.frameNames.next().value
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import DatabaseController from './database.js'
 import Controller from './controller.js'
 import Display from './display.js'
 import SpriteSheet from './SpriteSheet.js'
+import Animator from './Animator.js'
 import {loadImage} from './loaders.js'
 
 const dbc = new DatabaseController();
@@ -9,6 +10,9 @@ var gameCanvas = document.getElementById("gameCanvas")
 const display = new Display(gameCanvas, 1024, 800) 
 const controller = new Controller(gameCanvas)
 display.loadScene()
+var animator = 0
+var start = -1
+
 
 
 loadImage('../assets/tiles.png')
@@ -17,4 +21,31 @@ loadImage('../assets/tiles.png')
     sprites.define('ground', 0, 0);
     sprites.define('sky', 3, 23);
     sprites.draw('ground', gameCanvas.getContext('2d'), 16, 16)
+    animator = new Animator(sprites, 2, 1000)
+    requestAnimationFrame(update)
 });
+
+function update(time){
+    //console.log('beep')
+   // console.log(animator)
+    //console.log(animator.lastUpdateTime)
+        if (animator.lastUpdateTime == -1){
+            console.log('boop')
+            animator.lastUdateTime = time
+          }
+          //console.log("time: " + time)
+          
+          
+          const elapsed = time - animator.lastUpdateTime;
+          //console.log(elapsed)
+
+          if(elapsed >= animator.speed){
+              console.log("hi")
+              animator.currentStep += 1
+              var next = animator.frameNames.next().value
+              console.log(next)
+              if(next == undefined) animator.frameNames = animator.spriteSheet.tiles.keys()
+              animator.lastUdateTime = time
+          }
+        requestAnimationFrame(update)
+}
